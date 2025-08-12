1d578b9f0794e41fe7724d31d98dc276262b5d33
Mark Woodward <mark@markwoodward.com.au>
2025-08-12 22:15:44 +1000

    add @stylistic/eslint-plugin, config

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,31 @@ import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
+    extends: [stylistic.configs.recommended],
+    rules: {
+      // custom @stylistic rules
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/space-before-function-paren': ['error', 'always'],
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/arrow-parens': ['error', 'as-needed', {
+        requireForBlockBody: false
+      }],
+      // custom eslint rules
+      'vue/html-closing-bracket-newline': ['error', {
+        singleline: 'never',
+        multiline: 'never',
+        selfClosingTag: {
+          singleline: 'never',
+          multiline: 'always'
+        }
+      }]
+    }
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
@@ -15,16 +35,16 @@ export default defineConfig([
   {
     languageOptions: {
       globals: {
-        ...globals.browser,
-      },
-    },
+        ...globals.browser
+      }
+    }
   },
 
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  
+  ...pluginVue.configs['flat/strongly-recommended'],
+
   {
     ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
+    files: ['src/**/__tests__/*']
+  }
 ])
