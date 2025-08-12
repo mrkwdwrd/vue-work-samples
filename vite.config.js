8483c88f7012df3606d636e4079967dc9e252eaf
Mark Woodward <mark@markwoodward.com.au>
2025-08-12 22:39:43 +1000

    add mkcert

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools()
+    vueDevTools(),
+    mkcert()
   ],
   resolve: {
     alias: {
